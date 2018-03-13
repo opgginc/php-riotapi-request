998fdeb647c8df67423b6055d59002f7259718da
Sangrak Choi <kars@kargn.as>
2018-03-13 20:49:07 +0900

    API Scheme 도 지정 가능하도록 변경.

--- src/Constant/Platform.php
+++ src/Constant/Platform.php
@@ -11,6 +11,7 @@
 	class Platform
 	{
 		public static $API_HOST_PH = 'ph.api.riotgames.com';
+		public static $API_SCHEME_PH = 'https';
 
 		/**
 		 * @var string
@@ -19,7 +20,7 @@
 		public $region;
 
 		/** @var string */
-		public $platform, $apiHost;
+		public $platform, $apiHost, $apiScheme = 'https';
 
 		/**
 		 * @return Platform[]
@@ -47,146 +48,164 @@
 		}
 
 		public static function BR() {
-			$platform           = new static();
-			$platform->region   = 'BR';
-			$platform->platform = 'BR1';
-			$platform->apiHost  = 'br1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'BR';
+			$platform->platform  = 'BR1';
+			$platform->apiHost   = 'br1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function EUNE() {
-			$platform           = new static();
-			$platform->region   = 'EUNE';
-			$platform->platform = 'EUN1';
-			$platform->apiHost  = 'eun1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'EUNE';
+			$platform->platform  = 'EUN1';
+			$platform->apiHost   = 'eun1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function EUW() {
-			$platform           = new static();
-			$platform->region   = 'EUW';
-			$platform->platform = 'EUW1';
-			$platform->apiHost  = 'euw1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'EUW';
+			$platform->platform  = 'EUW1';
+			$platform->apiHost   = 'euw1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function JP() {
-			$platform           = new static();
-			$platform->region   = 'JP';
-			$platform->platform = 'JP1';
-			$platform->apiHost  = 'jp1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'JP';
+			$platform->platform  = 'JP1';
+			$platform->apiHost   = 'jp1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function KR() {
-			$platform           = new static();
-			$platform->region   = 'KR';
-			$platform->platform = 'KR';
-			$platform->apiHost  = 'kr.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'KR';
+			$platform->platform  = 'KR';
+			$platform->apiHost   = 'kr.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function LAN() {
-			$platform           = new static();
-			$platform->region   = 'LAN';
-			$platform->platform = 'LA1';
-			$platform->apiHost  = 'la1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'LAN';
+			$platform->platform  = 'LA1';
+			$platform->apiHost   = 'la1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function LAS() {
-			$platform           = new static();
-			$platform->region   = 'LAS';
-			$platform->platform = 'LA2';
-			$platform->apiHost  = 'la2.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'LAS';
+			$platform->platform  = 'LA2';
+			$platform->apiHost   = 'la2.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function NA() {
-			$platform           = new static();
-			$platform->region   = 'NA';
-			$platform->platform = 'NA1';
-			$platform->apiHost  = 'na1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'NA';
+			$platform->platform  = 'NA1';
+			$platform->apiHost   = 'na1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function OCE() {
-			$platform           = new static();
-			$platform->region   = 'OCE';
-			$platform->platform = 'OC1';
-			$platform->apiHost  = 'oc1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'OCE';
+			$platform->platform  = 'OC1';
+			$platform->apiHost   = 'oc1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function TR() {
-			$platform           = new static();
-			$platform->region   = 'TR';
-			$platform->platform = 'TR1';
-			$platform->apiHost  = 'tr1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'TR';
+			$platform->platform  = 'TR1';
+			$platform->apiHost   = 'tr1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function RU() {
-			$platform           = new static();
-			$platform->region   = 'RU';
-			$platform->platform = 'RU';
-			$platform->apiHost  = 'ru.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'RU';
+			$platform->platform  = 'RU';
+			$platform->apiHost   = 'ru.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function SG() {
-			$platform           = new static();
-			$platform->region   = 'SG';
-			$platform->platform = 'SG';
-			$platform->apiHost  = 'sg.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'SG';
+			$platform->platform  = 'SG';
+			$platform->apiHost   = 'sg.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function ID() {
-			$platform           = new static();
-			$platform->region   = 'ID';
-			$platform->platform = 'ID';
-			$platform->apiHost  = 'id1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'ID';
+			$platform->platform  = 'ID';
+			$platform->apiHost   = 'id1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function PH() {
-			$platform           = new static();
-			$platform->region   = 'PH';
-			$platform->platform = 'PH';
-			$platform->apiHost  = static::$API_HOST_PH;
+			$platform            = new static();
+			$platform->region    = 'PH';
+			$platform->platform  = 'PH';
+			$platform->apiHost   = static::$API_HOST_PH;
+			$platform->apiScheme = static::$API_SCHEME_PH;
 			return $platform;
 		}
 
 		public static function TW() {
-			$platform           = new static();
-			$platform->region   = 'TW';
-			$platform->platform = 'TW';
-			$platform->apiHost  = 'tw.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'TW';
+			$platform->platform  = 'TW';
+			$platform->apiHost   = 'tw.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function VN() {
-			$platform           = new static();
-			$platform->region   = 'VN';
-			$platform->platform = 'VN';
-			$platform->apiHost  = 'vn.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'VN';
+			$platform->platform  = 'VN';
+			$platform->apiHost   = 'vn.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function TH() {
-			$platform           = new static();
-			$platform->region   = 'TH';
-			$platform->platform = 'TH';
-			$platform->apiHost  = 'th.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'TH';
+			$platform->platform  = 'TH';
+			$platform->apiHost   = 'th.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 
 		public static function PBE() {
-			$platform           = new static();
-			$platform->region   = 'PBE';
-			$platform->platform = 'PBE1';
-			$platform->apiHost  = 'pbe1.api.riotgames.com';
+			$platform            = new static();
+			$platform->region    = 'PBE';
+			$platform->platform  = 'PBE1';
+			$platform->apiHost   = 'pbe1.api.riotgames.com';
+			$platform->apiScheme = 'https';
 			return $platform;
 		}
 	}
