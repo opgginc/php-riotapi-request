b66cb580053414fc3cad4564a05ed7fe64d8d594
최석준 <seokjun.choi@op.gg>
2023-01-06 06:23:46 +0900

    SEA (garena) 리전 추가

--- src/Constant/Platform.php
+++ src/Constant/Platform.php
@@ -38,12 +38,13 @@
 				static::OCE(),
 				static::TR(),
 				static::RU(),
-//				static::SG(),
-//				static::ID(),
-//				static::PH(),
-//				static::TW(),
-//				static::VN(),
-//				static::TH(),
+
+                static::PH(),
+                static::SG(),
+                static::TW(),
+                static::TH(),
+                static::VN(),
+
 				static::AMERICAS(),
 				static::ASIA(),
 				static::EUROPE(),
@@ -149,59 +150,52 @@
 			return $platform;
 		}
 
-		public static function SG() {
-			$platform            = new static();
-			$platform->region    = Region::SG;
-			$platform->platform  = 'SG';
-			$platform->apiHost   = 'sg.api.riotgames.com';
-			$platform->apiScheme = 'https';
-			return $platform;
-		}
+        public static function PH() {
+            $platform            = new static();
+            $platform->region    = Region::PH;
+            $platform->platform  = 'PH2';
+            $platform->apiHost   = 'ph2.api.riotgames.com';
+            $platform->apiScheme = 'https';
+            return $platform;
+        }
 
-		public static function ID() {
-			$platform            = new static();
-			$platform->region    = Region::ID;
-			$platform->platform  = 'ID';
-			$platform->apiHost   = 'id1.api.riotgames.com';
-			$platform->apiScheme = 'https';
-			return $platform;
-		}
+        public static function SG() {
+            $platform            = new static();
+            $platform->region    = Region::SG;
+            $platform->platform  = 'SG2';
+            $platform->apiHost   = 'sg2.api.riotgames.com';
+            $platform->apiScheme = 'https';
+            return $platform;
+        }
 
-		public static function PH() {
-			$platform            = new static();
-			$platform->region    = Region::PH;
-			$platform->platform  = 'PH';
-			$platform->apiHost   = static::$API_HOST_PH;
-			$platform->apiScheme = 'static::$API_SCHEME_PH';
-			return $platform;
-		}
+        public static function TW() {
+            $platform            = new static();
+            $platform->region    = Region::TW;
+            $platform->platform  = 'TW2';
+            $platform->apiHost   = 'tw2.api.riotgames.com';
+            $platform->apiScheme = 'https';
+            return $platform;
+        }
 
-		public static function TW() {
-			$platform            = new static();
-			$platform->region    = Region::TW;
-			$platform->platform  = 'TW';
-			$platform->apiHost   = 'tw.api.riotgames.com';
-			$platform->apiScheme = 'https';
-			return $platform;
-		}
+        public static function TH() {
+            $platform            = new static();
+            $platform->region    = Region::TH;
+            $platform->platform  = 'TH2';
+            $platform->apiHost   = 'th2.api.riotgames.com';
+            $platform->apiScheme = 'https';
+            return $platform;
+        }
 
-		public static function VN() {
-			$platform            = new static();
-			$platform->region    = Region::VN;
-			$platform->platform  = 'VN';
-			$platform->apiHost   = 'vn.api.riotgames.com';
-			$platform->apiScheme = 'https';
-			return $platform;
-		}
+        public static function VN() {
+            $platform            = new static();
+            $platform->region    = Region::VN;
+            $platform->platform  = 'VN2';
+            $platform->apiHost   = 'vn2.api.riotgames.com';
+            $platform->apiScheme = 'https';
+            return $platform;
+        }
 
-		public static function TH() {
-			$platform            = new static();
-			$platform->region    = Region::TH;
-			$platform->platform  = 'TH';
-			$platform->apiHost   = 'th.api.riotgames.com';
-			$platform->apiScheme = 'https';
-			return $platform;
-		}
+        /////////////////////////////////////////
 
 		public static function PBE() {
 			$platform            = new static();
